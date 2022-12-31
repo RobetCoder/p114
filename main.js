025cd3aa8bdd7842b1470bac71315b5f32c5fa5a
RobetCoder <93022804+RobetCoder@users.noreply.github.com>
2022-12-31 12:43:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.center();}
+
+    function take_snapshot(){
+    save('myFilter.png');}
+    
+    function draw(){}
